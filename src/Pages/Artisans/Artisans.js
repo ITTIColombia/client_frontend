db59465ad40d9138644278dc4902f7b21767fc52
Juan David Serrano <jd.serranom@uniandes.edu.co>
2022-03-22 16:02:52 +0100

    FInal changes

--- src/Pages/Artisans/Artisans.js
+++ src/Pages/Artisans/Artisans.js
@@ -2,9 +2,9 @@ import './Artisans.css';
 import Topbar from "../../Components/Topbar/Topbar";
 import Navbar from "../../Components/Navbar/Navbar";
 import {useEffect, useState} from "react";
-import ArtisansMockup from "../../models_mockup/Artisan/Artisans";
+import ArtisansMockup from "../../Mockup/Artisan/Artisans";
 import {FormattedMessage} from "react-intl";
-require("../../Assets/Map/")
+import completeMap from '../../Assets/Map/CompleteMap.svg';
 
 
 
@@ -25,16 +25,18 @@ function Artisans(){
             <div className="row intro-artisans">
                 <div className="col-6 description-artisans">
                     <h1 id="title-artisans"><FormattedMessage id="Artisans"/></h1>
-                    <p className="normal-text"><FormattedMessage id="ArtisansDescription"/></p>
+                    <p className="text-artisans"><FormattedMessage id="ArtisansDescription"/></p>
                     <div className="filter-artisans">
-                        <h3 className="orange normal-text text-uppercase"><FormattedMessage id="Filters"/></h3>
+                        <h3 className="orange text-artisans text-uppercase"><FormattedMessage id="Filters"/></h3>
                     </div>
                 </div>
                 <div className="col-6">
-                    <img/>
+                    <div className="float-end">
+                        <img id="colombia_map" src={completeMap} alt="colombia-map"/>
+                    </div>
                 </div>
             </div>
-            <div className="artisans-section row">
+            <div className="artisans-section row float-left">
                 {artisans.map((artisan,i) =>{
                     return <img className="col-4" src={artisan.profilePhoto} alt={"artisan"+i} key={"artisan"+i}/>
                 })}
