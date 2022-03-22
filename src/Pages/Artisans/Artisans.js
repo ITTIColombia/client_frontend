b82bc6de93c6fbf6e0c8d50fd97674c0814df5c6
Juan David Serrano <jd.serranom@uniandes.edu.co>
2022-03-22 08:48:39 +0100

    Created export

---
+++ src/Pages/Artisans/Artisans.js
@@ -0,0 +1,21 @@
+import './Artisans.css';
+import Topbar from "../../Components/Topbar/Topbar";
+import Navbar from "../../Components/Navbar/Navbar";
+
+
+
+function Artisans(){
+
+
+
+    return(
+        <div className="Artisans">
+            <Topbar/>
+            <Navbar/>
+
+        </div>
+    )
+}
+
+
+export default Artisans;
