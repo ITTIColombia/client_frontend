2939ed8fd974de62443919cae3d9ee02f99baa6e
Juan David Serrano <jd.serranom@uniandes.edu.co>
2022-03-23 21:53:41 +0100

    Finished artisan page with internacionalization

--- src/Pages/Artisans/Artisans.js
+++ src/Pages/Artisans/Artisans.js
@@ -4,13 +4,19 @@ import Navbar from "../../Components/Navbar/Navbar";
 import {useEffect, useState} from "react";
 import ArtisansMockup from "../../Mockup/Artisan/Artisans";
 import {FormattedMessage} from "react-intl";
-
-
+import ReusableFilter from "../../Components/Filters/ReusableFilter";
 
 
 function Artisans(){
 
+    const optionsSearchForm = {"Region": ["Pacific", "Caribbean", "Andean", "Orinoquia", "Amazonian"],
+        "Type": ["Indigenous", "PopularTraditional", "Contemporary"],
+        "Job": ["JewelerySilversmith", "PotteryCeramicGlass", "KnittingEmbroidery", "BasketryHats", "WoodWork", "Leather"]}
+
     const [artisans, setArtisans] = useState(ArtisansMockup)
+
+    const [searchForm, setSearchForm] = useState(
+        {"Region":"DEFAULT", "Type": "DEFAULT", "Job": "DEFAULT"})
 
     useEffect(()=>{
         console.log(artisans)
@@ -26,12 +32,12 @@ function Artisans(){
                     <h1 id="title-artisans"><FormattedMessage id="Artisans"/></h1>
                     <p className="text-artisans"><FormattedMessage id="ArtisansDescription"/></p>
                     <div className="filter-artisans">
-                        <h3 className="orange text-artisans text-uppercase"><FormattedMessage id="Filters"/></h3>
+                        <ReusableFilter options={optionsSearchForm} state={searchForm} setState={setSearchForm}/>
                     </div>
                 </div>
                 <div className="col-6">
                     <div className="float-end">
-                        <img id="colombia_map" src='/Assets/Map/CompleteMap.svg' alt="colombia-map"/>
+                        <img id="colombia_map" src='/Assets/Map/MapRegions.png' alt="colombia-map"/>
                     </div>
                 </div>
             </div>
