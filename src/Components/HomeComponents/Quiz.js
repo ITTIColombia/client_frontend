525c5f04d029d66aeaa85ce25f0cd079ff592fc1
Juan David Serrano <jd.serranom@uniandes.edu.co>
2022-04-29 09:59:55 +0200

    Modified questions to be from mockup

--- src/Components/HomeComponents/Quiz.js
+++ src/Components/HomeComponents/Quiz.js
@@ -5,8 +5,12 @@ import QuizQuestion from './QuizQuestion'
 import Question1 from '../../Mockup/QuizQuestions/QuizQuestion1.json';
 import Question2 from '../../Mockup/QuizQuestions/QuizQuestion2.json';
 import Question3 from '../../Mockup/QuizQuestions/QuizQuestion3.json';
+import AppContext from "../../AppContext";
+import {useContext} from "react";
 
 function Quiz() {
+
+    const context = useContext(AppContext);
 
     let userLang = navigator.language || navigator.userLanguage;
 
@@ -25,16 +29,22 @@ function Quiz() {
             </div>
             <div className='quiz-questions'>
                 <div className='quiz-block'>
-                    <img className='quiz-photo' src={"/Images/Mockup/QuizQuestions/Foto1.png"} alt="photo1quiz"/>
-                    <QuizQuestion question="Hola?" answer1="si" answer2="no"/>
+                    <img className='quiz-photo' src={question1.photo} alt="photo1quiz"/>
+                    <QuizQuestion question={context.languageSettings.locale.startsWith("en") ? question1.questionEN: question1.questionES}
+                                  answer1={context.languageSettings.locale.startsWith("en") ? question1.option1EN: question1.option1ES}
+                                  answer2={context.languageSettings.locale.startsWith("en") ? question1.option2EN: question1.option2ES} />
                 </div>
                 <div className='quiz-block'>
-                    <img className='quiz-photo' src={"/Images/Mockup/QuizQuestions/Foto1.png"} alt="photo1quiz"/>
-                    <QuizQuestion question="Hola?" answer1="si" answer2="no"/>
+                    <img className='quiz-photo' src={question2.photo} alt="photo2quiz"/>
+                    <QuizQuestion question={context.languageSettings.locale.startsWith("en") ? question2.questionEN: question2.questionES}
+                                  answer1={context.languageSettings.locale.startsWith("en") ? question2.option1EN: question2.option1ES}
+                                  answer2={context.languageSettings.locale.startsWith("en") ? question2.option2EN: question2.option2ES} />
                 </div>
                 <div className='quiz-block'>
-                    <img className='quiz-photo' src={"/Images/Mockup/QuizQuestions/Foto1.png"} alt="photo1quiz"/>
-                    <QuizQuestion question="Hola?" answer1="si" answer2="no"/>
+                    <img className='quiz-photo' src={question3.photo} alt="photo3quiz"/>
+                    <QuizQuestion question={context.languageSettings.locale.startsWith("en") ? question3.questionEN: question3.questionES}
+                                  answer1={context.languageSettings.locale.startsWith("en") ? question3.option1EN: question3.option1ES}
+                                  answer2={context.languageSettings.locale.startsWith("en") ? question3.option2EN: question3.option2ES}/>
                 </div>
             </div>
         </div>
