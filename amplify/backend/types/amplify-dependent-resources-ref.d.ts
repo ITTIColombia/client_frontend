fc1ca8c4c6ea1d155f928184ba63090c8e40ad99
Juan David Serrano <jd.serranom@uniandes.edu.co>
2022-05-03 11:29:49 +0200

    Added team again and created cognito

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "clientfrontend19ecb3a0": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3636d6643": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
