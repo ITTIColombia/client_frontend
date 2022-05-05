f5c2b646bdc2dd580f4c8a50c25d6f4b4299f99e
Juan David Serrano <jd.serranom@uniandes.edu.co>
2022-05-05 14:49:51 +0200

    Footer finished

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,34 +4,35 @@ import "./Footer.css";
 
 function Footer() {
     return (
-        <div className="Footer">
-            <div className="footer-content">
-            <div className="footer-logo">
-                <img className="img" src="/Assets/Logos/logoFooter.svg" alt="ITTI" />
-                <div className="footer-copy">
-                    <img className="copy" src="/Assets/Icons/copyright.svg" alt="copyright"/>
-                    <p>2021 ITTI. All Rights Reserved.</p>
+        <React.Fragment>
+            <div id="Footer" className="container-fluid">
+                <div className="row footer-row">
+                    <div className="col-lg-4 col-sm-12 col-xs-12 footer-icon-col footer-col">
+                        <img className="img" src="/Assets/Logos/logoFooter.svg" alt="ITTI"/>
+                        <div id="footer-copyright-region">
+                            <img className="copy" src="/Assets/Icons/copyright.svg" alt="copyright"/>
+                            <span>2021 ITTI. <FormattedMessage id="AllRightsReserved"/></span>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-sm-12 col-xs-12 footer-col">
+                        <p className="footer-section-title"><FormattedMessage id="MeetUs"/></p>
+                        <p className="footer-section-content"><FormattedMessage id="AboutUsFooter"/></p>
+                        <p className="footer-section-content"><FormattedMessage id="FAQ"/></p>
+                    </div>
+                    <div className=" col-lg-3 col-sm-12 col-xs-12  footer-col">
+                        <p className="footer-section-title"><FormattedMessage id="ContactUs"/></p>
+                        <p className="footer-section-content"><a href="mailto:somositti@gmail.com">somositti@gmail.com</a></p>
+                        <p className="footer-section-content"><a href="tel:+573144840591">+57 314 4840591</a></p>
+                    </div>
+                    <div className="col-lg-2 col-sm-12 col-xs-12 footer-col">
+                        <p className="footer-section-title"><FormattedMessage id="FollowUs"/></p>
+                        <p className="footer-section-content"><a href="https://www.instagram.com/somositti/" target="_blank" rel="noreferrer">Instagram</a></p>
+                        <p className="footer-section-content"><a href="https://www.youtube.com/channel/UC67QLsZD9ftLuXyVH4e-fkA" target="_blank" rel="noreferrer">Youtube</a></p>
+                    </div>
                 </div>
             </div>
-            <div className="footer-info">
-                <div className="footer-info-item">
-                    <p className="footer-info-tit"><FormattedMessage id="MeetUs"/></p>
-                    <p><FormattedMessage id="AboutUsFooter"/></p>
-                    <p><FormattedMessage id="FAQ"/></p>
-                </div>
-                <div className="footer-info-item">
-                    <p className="footer-info-tit"><FormattedMessage id="ContactUs"/></p>
-                    <a href="mailto:somositti@gmail.com">somositti@gmail.com</a>
-                    <a href="tel:+573144840591">+57 314 4840591</a>
-                </div>
-                <div className="footer-info-item">
-                    <p className="footer-info-tit"><FormattedMessage id="FollowUs"/></p>
-                    <a href="https://www.instagram.com/somositti/" target="_blank" rel="noreferrer">Instagram</a>
-                    <a href="https://www.youtube.com/channel/UC67QLsZD9ftLuXyVH4e-fkA" target="_blank" rel="noreferrer">Youtube</a>
-                </div>
-            </div>
-            </div>
-        </div>
+        </React.Fragment>
+
     )
 }
 
