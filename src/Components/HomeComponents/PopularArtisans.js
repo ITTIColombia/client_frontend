a549aebde60e04a00c73a800e351d3aa2383c6d6
Juan David Serrano <jd.serranom@uniandes.edu.co>
2022-05-31 18:33:05 +0200

    Added enpoint sample artisans

--- src/Components/HomeComponents/PopularArtisans.js
+++ src/Components/HomeComponents/PopularArtisans.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
 import "./PopularArtisans.css"; 
 
 function PopularArtisans() {
+
+    const [artisans, setArtisans] = useState([])
+
+
+
+    useEffect(()=> {
+        fetch(`${process.env.REACT_APP_BACKEND_URL}/artisans/sample?qty=4`, {
+            method: 'GET',
+            headers: {'Content-Type': 'application/json'}
+        }).then((res)=>{
+            if(res.status === 200){
+                res.json().then(json=>{
+                    setArtisans(json)
+                }).catch(err=>{
+                    console.log("Error extracting json:", err)
+                })
+            }else if(res.status === 204){
+                setArtisans([])
+            }
+        }).catch(err=>{
+            console.log("Error receiving artisans:", err)
+        })
+    }, [])
+
+
     return (
         <div className="PopularArtisans" id="popularArtisans">
             <div className="popularArtisans-tit">
@@ -12,10 +37,14 @@ function PopularArtisans() {
                 }}/></h3>
             </div>
             <div className="popularArtisans-content">
-                <Link to="/artesanos/:_1"><img className="img" src={"/Images/Mockup/Artisan/Artisan1.png"} alt="Artesano 1" /></Link>
-                <Link to="/artesanos/:_2"><img className="img" src={ "/Images/Mockup/Artisan/Artisan2.png"} alt="Artesano 2" /></Link>
-                <Link to="/artesanos/:_1"><img className="img" src={ "/Images/Mockup/Artisan/Artisan3.png"} alt="Artesano 3" /></Link>
-                <Link to="/artesanos/:_2"><img className="img" src={ "/Images/Mockup/Artisan/Artisan4.png"} alt="Artesano 4" /></Link>
+                {artisans.map((artisan,i)=>{
+                    const contentURL = `https://s3.amazonaws.com/${process.env.REACT_APP_BUCKET_ID}/artisans/${artisan['_id']}`
+                    return(
+                        <Link to={`/artesanos/${artisan["_id"]}`}>
+                            <img className="img" src={`${contentURL}/${artisan.media?.profile}`} alt="Artesano 1" />
+                        </Link>
+                    )
+                })}
             </div>
             <div className="popularArtisans-but-container">
                     <div className="popularArtisans-but-line">
